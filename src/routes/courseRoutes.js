ae2f7548b2625e177b90e23d5116132434ca6f26
fatima-azzahra2003 <Fatimaazzahrahamdaoui3@gmail.com>
2025-01-07 00:37:51 +0100

    Update courseRoutes.js

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -12,4 +12,5 @@ router.post('/', courseController.createCourse);
 router.get('/:id', courseController.getCourse);
 router.get('/stats', courseController.getCourseStats);
 
+
 module.exports = router;
