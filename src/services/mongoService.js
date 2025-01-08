3ed859c19b1eec4b678b6e571f5bbe95eb884b17
fatima-azzahra2003 <Fatimaazzahrahamdaoui3@gmail.com>
2025-01-08 21:02:36 +0100

    Update mongoService.js

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -6,9 +6,16 @@ const { ObjectId } = require('mongodb');
 // Fonctions utilitaires pour MongoDB
 async function findOneById(collection, id) {
   // TODO: Implémenter une fonction générique de recherche par ID
+  return db.collection(collection).findOne({ _id: new ObjectId(id) });
+}
+async function insertOne(collection, data) {
+  return db.collection(collection).insertOne(data);
 }
 
 // Export des services
 module.exports = {
   // TODO: Exporter les fonctions utilitaires
+  findOneById,
+  insertOne
 };
+
