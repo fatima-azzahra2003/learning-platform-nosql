6c51b8a0331568b075bcc7ad52506ef59c5cb355
fatima-azzahra2003 <Fatimaazzahrahamdaoui3@gmail.com>
2025-01-08 21:03:05 +0100

    Update redisService.js

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -4,10 +4,19 @@
 // Réponse :
 
 // Fonctions utilitaires pour Redis
+const { redisClient } = require('../config/db');
 async function cacheData(key, data, ttl) {
     // TODO: Implémenter une fonction générique de cache
+    await redisClient.setEx(key, ttl, JSON.stringify(data));
+  }
+  
+  async function getData(key) {
+    const data = await redisClient.get(key);
+    return data ? JSON.parse(data) : null;
   }
   
   module.exports = {
     // TODO: Exporter les fonctions utilitaires
+    cacheData,
+    getData,
   };
