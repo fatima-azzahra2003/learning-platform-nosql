59f13feef4eb684cd84ddc92d8f11cce19cdd464
fatima-azzahra2003 <Fatimaazzahrahamdaoui3@gmail.com>
2025-01-12 20:01:56 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,19 +3,29 @@
 
 const express = require('express');
 const config = require('./config/env');
-const db = require('./config/db');
+const { connectMongo, connectRedis, closeConnections } = require('./config/db');
 
 const courseRoutes = require('./routes/courseRoutes');
-const studentRoutes = require('./routes/studentRoutes');
+
 
 const app = express();
-
+app.use(express.json());
+app.use('/courses', courseRoutes);
 async function startServer() {
   try {
     // TODO: Initialiser les connexions aux bases de données
     // TODO: Configurer les middlewares Express
     // TODO: Monter les routes
     // TODO: Démarrer le serveur
+    await db.connectMongo();
+    await db.connectRedis();
+  
+    app.use(express.json());
+    app.use('/api/courses', courseRoutes);
+  
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -25,6 +35,9 @@ async function startServer() {
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
   // TODO: Implémenter la fermeture propre des connexions
+  console.log('Shutting down server...');
+  await closeConnections();
+  process.exit(0);
 });
 
 startServer();
