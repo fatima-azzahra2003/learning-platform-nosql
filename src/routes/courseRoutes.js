437497aeb0fdacb652186d31c6896bca3869f33d
fatima-azzahra2003 <Fatimaazzahrahamdaoui3@gmail.com>
2025-01-12 20:00:49 +0100

    Update courseRoutes.js

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -6,11 +6,11 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
-
-// Routes pour les cours
-router.post('/', courseController.createCourse);
-router.get('/:id', courseController.getCourse);
 router.get('/stats', courseController.getCourseStats);
+router.get('/delete', courseController.deletecourse);
+// Routes pour les cours
+router.get('/:id', courseController.getCourse);
 
+router.post('/', courseController.createCourse);
 
 module.exports = router;
